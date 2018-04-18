dc50e63c33470f099f20d891a0432f15e92283b2
yohanym95 <malshikay@gmail.com>
2018-04-18 18:32:46 +0530

    Record_Book

---
+++ src/recordbook/RecordBook.java
@@ -0,0 +1,35 @@
+
+package recordbook;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+
+
+
+public class RecordBook {
+
+   
+public static Connection connect(){
+    
+    Connection con=null;
+    
+    String url="jdbc:mysql://localhost:3306/recordbook";
+    String user="root";
+    String password="";
+    
+    try{
+        Class.forName("com.mysql.jdbc.Driver");//driver loaction
+        con = (Connection) DriverManager.getConnection(url, user, password);//get connection
+        
+    }catch(Exception e){
+        System.out.println(e);
+    }
+    
+    
+    
+    return con;
+}
+    
+}
+
+
